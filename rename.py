e039de336afdf99e9d0f41bd900c2c3d5af7f959
Dan Nelson <danelson88@gmail.com>
2017-01-15 22:40:08 -0600

    Conform to pep8

--- rename.py
+++ rename.py
@@ -1,20 +1,15 @@
-#rename.py
-
 import os
 import sys
 
-path1 = "/Users/danelson/Desktop/images/"
-path2 = "/Users/danelson/Desktop/images2/"
+old_path = os.path.abspath(sys.argv[1])
+dir_name, base_name = os.path.split(old_path)
+new_path = os.path.join(dir_name, base_name + "_renamed")
 
-old_full_path = os.path.abspath(sys.argv[1])
-dirname, basename = os.path.split(old_full_path)
-new_full_path = os.path.join(dirname, basename+"_renamed")
+dir_ = os.listdir(old_path)
+os.mkdir(new_path)
 
-dir = os.listdir(old_full_path)
-os.mkdir(new_full_path)
-
-for i, file in enumerate(dir):
-	new_name = "img-{05d}.jpg".format(i)
-    old = os.path.join(old_full_path, file)
-    new = os.path.join(old_full_path, new_name)
-	os.rename(old_full_path,new_full_path)
+for i, file_ in enumerate(dir_):
+    new_name = "img-{05d}.jpg".format(i)
+    old = os.path.join(old_path, file_)
+    new = os.path.join(old_path, new_name)
+    os.rename(old_path, new_path)
